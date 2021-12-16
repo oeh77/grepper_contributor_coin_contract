60fdc8f76e3afa0f38b4591d801e8d5a21b25cd5
taylor hawkes <taylorhawkes@taylors-MacBook-Pro.local>
2021-12-16 11:59:08 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# grepper_contributor_coin_contract
+How Grepper Contributor Coin (GRPC) works.
+Coins will be minted on a daily basis, to users who have contributed helpful answers/content to Grepper. Initially 1 GRPC will be issued per answer per day for answers that rank in the top 30% quality score.  Additionally, in order to prevent spam answers, for  “bad answers” (answers that have a quality score in the bottom 20% of answers) 1 GRPC will be deducted from that day's earnings. 
+
+Ex: Sally has inputted 20 Grepper answers, 10 of which have a quality score that fall within the top 30% of answers and 2 of which fall within the bottom 20% of answers. Sally will earn 8 newly minted GRPC per day.  If Sally sets the 2 bad answers to private or deletes them, then those answers will not be deducted from her earnings, so she will earn 10 GRPC a day.  Users can’t earn negative coins daily, so if a user only has 2 answers and they are both bad, they will simply earn 0 GRPC a day. 
+
+Good Answers (Top 30%): At present in order to be in the top 30% an answer must have a quality score of .93 or about ~1. This number should increase slightly as the quality score of all answers increase. 
+
+Bad Answers (Bottom 20%) : At present this number is a quality score of less than ~0. This number should increase slightly as the quality score of all answers increase. 
+
+Quality Score: The thrust of an answers “quality score” is to get a number on how “helpful” an answer is to other developers, this is a little tricky and may be adjusted in order to better capture “helpfulness”. At present an answer's “quality score” is calculated based on a weighted number of upvotes, downvotes and copies it gets relative to views . Quality Score = (upvotes*upvoteWeight) + (copies*copyWeight) - (downvotes*downvoteWeight ) / (views) 
+
+
+
+The Donation System 
+Anyone who is helped by a Grepper answer or wants to support the community can donate to all “Grepper Contributors” by donating to a “smart contract” found {here}. This contract holds SOL and any holder of GRPC can swap their GRPC for SOL based on the amount of SOL held in the contract and the amount of GRPC outstanding.  Ex: Let’s say sally has 5,000 GRPC and wants to swap them for SOL. If at the time there are 1,000,000GRPC outstanding and the contract holds 10SOL.  Sally can Swap her 5k GRPC for .05 SOL (.05 = 10/1000000 * 5000) 
+
+Note: SOL can be swapped for almost any currency on a number of exchanges such as coinbase. 
+
+
---
+++ program-rust/src/lib.rs
@@ -0,0 +1,104 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    program::{invoke_signed,invoke},
+};
+
+use spl_token::{
+    check_program_account,
+    error::TokenError,
+    instruction::{
+        approve, transfer,initialize_account, initialize_mint, mint_to, revoke, set_authority, AuthorityType, burn
+    },
+    solana_program::program_pack::Pack,
+    state::{Account, AccountState, Mint, Multisig},
+};
+use std::str::FromStr;
+use std::convert::TryInto;
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey, 
+    accounts: &[AccountInfo], 
+    _instruction_data: &[u8], 
+) -> ProgramResult {
+ 
+    let (&tag, rest) = _instruction_data.split_first().ok_or(ProgramError::InvalidInstructionData)?;
+    let amount = rest.get(..8).and_then(|slice|slice.try_into().ok()).map(u64::from_le_bytes).ok_or(ProgramError::InvalidInstructionData)?;
+
+    let account_info_iter = &mut accounts.iter();
+
+    let our_token_program = next_account_info(account_info_iter)?;
+
+    //very important we hard code this to be correct because otherwise they canpass a different supply
+    let our_token_program_key=Pubkey::from_str("GLoCBYmGqqUCZLy6XpmLUF9xAweUfe9R35pinscKzdZh").unwrap();
+    if *our_token_program.key != our_token_program_key{
+        msg!("Incorrect Token Program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let main_token_program = next_account_info(account_info_iter)?;
+    if *main_token_program.key != spl_token::id(){
+          msg!("Incorrect token program");
+          return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let initializers_main_account = next_account_info(account_info_iter)?;
+    if !initializers_main_account.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let initializers_token_account = next_account_info(account_info_iter)?;
+    if *initializers_token_account.owner != *main_token_program.key{
+        msg!("Incorrect owner of token account");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let contract_holder_mainid = next_account_info(account_info_iter)?;
+    if *contract_holder_mainid.owner != *program_id{
+        msg!("Incorrect contract holder");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let contract_holder_gcid = next_account_info(account_info_iter)?;
+    if *contract_holder_gcid.owner != *main_token_program.key{
+        msg!("Incorrect contract token holder");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+
+
+    let mint = Mint::unpack(&our_token_program.data.borrow()).map_err(|_| ProgramError::InvalidInstructionData)?;
+
+    let contractamportsBalance = contract_holder_mainid.lamports();
+    let gcCoinsPerLamport =  mint.supply as f64 / contract_holder_mainid.lamports() as f64  ;
+    let lamports_to_send_float =  amount as f64 / gcCoinsPerLamport as f64;
+    let lamports_to_send = lamports_to_send_float as  u64;
+
+    //so first we burn our spl token
+    let ix = spl_token::instruction::burn(
+        main_token_program.key,
+        initializers_token_account.key,
+        our_token_program.key,
+        initializers_main_account.key,
+        &[&initializers_main_account.key],
+        amount,
+    )?;
+
+     invoke(
+          &ix,
+          &[initializers_token_account.clone(), our_token_program.clone(), initializers_main_account.clone(),main_token_program.clone()]
+     );
+
+    //send swapper the SOL
+    **contract_holder_mainid.try_borrow_mut_lamports()? -= lamports_to_send;
+    **initializers_main_account.try_borrow_mut_lamports()? += lamports_to_send;
+
+    Ok(())
+}
